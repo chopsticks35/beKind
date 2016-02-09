cf6dcf90060b75aef900ff6a555e0703dbbf3611
Ti Wegmeyer <tiwegmeyeryoga@gmail.com>
2016-02-08 17:00:04 -0700

    adding retail section

---
+++ script.js
@@ -0,0 +1,10 @@
+//Studio constructor
+var Studio = function(name, staff, clients, classes, reports, retail){
+    this.name = name;
+    this.staff = staff;
+    this.clients = clients;
+    this.classes = classes;
+    this.reports = reports;
+    this.retail = retail;
+}
+
