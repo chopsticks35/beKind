e53563aff18fe025a38ca8f4da7926e39f090c2e
Ti Wegmeyer <tiwegmeyeryoga@gmail.com>
2016-02-09 23:34:30 -0700

    adding reports function

--- mainScript.js
+++ mainScript.js
@@ -1,7 +1,31 @@
-angular.module('myApp', []);
+angular.module("myApp", ["ng-fusioncharts"])
 
-angular.module('myApp')
-    .controller('myController', ['$scope', function($scope){
-
-               
-}])
+angular.module("myApp")
+    .controller("myController", function ($scope) {
+              $scope.myDataSource = {
+                chart: {
+                    caption: "February Attendance",
+                    subCaption: "Sorted by Teacher",
+                },
+                data:[{
+                    label: "Ti Wegmeyer",
+                    value: "150"
+                },
+                {
+                    label: "Keturah Stewart",
+                    value: "100"
+                },
+                {
+                    label: "Moniqua Acosta",
+                    value: "78"
+                },
+                {
+                    label: "Deena Gilbert",
+                    value: "75"
+                },
+                {
+                    label: "Robin Goldsmith",
+                    value: "62"
+                }]
+              };
+            });
