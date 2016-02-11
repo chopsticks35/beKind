a982e280b6351b8c5c622fb8ef300c207f749f0c
Ti Wegmeyer <tiwegmeyeryoga@gmail.com>
2016-02-10 23:40:32 -0700

    working on form

--- mainScript.js
+++ mainScript.js
@@ -1,31 +1,21 @@
-angular.module("myApp", ["ng-fusioncharts"])
+angular.module("myApp", [])
 
 angular.module("myApp")
     .controller("myController", function ($scope) {
-              $scope.myDataSource = {
-                chart: {
-                    caption: "February Attendance",
-                    subCaption: "Sorted by Teacher",
-                },
-                data:[{
-                    label: "Ti Wegmeyer",
-                    value: "150"
-                },
-                {
-                    label: "Keturah Stewart",
-                    value: "100"
-                },
-                {
-                    label: "Moniqua Acosta",
-                    value: "78"
-                },
-                {
-                    label: "Deena Gilbert",
-                    value: "75"
-                },
-                {
-                    label: "Robin Goldsmith",
-                    value: "62"
-                }]
-              };
+    
+            //contact-form binding
+            $scope.myForm = {};
+            $scope.myForm.name = "";
+            $scope.myForm.email = "";
+            $scope.myForm.hear = "";
+            $scope.myForm.wantNewsletter;
+            $scope.myForm.message = "";
+            
+            $scope.myForm.options = [
+                {id: "referrals", name: "Referrals"},
+                {id: "web", name: "Web Search"},
+                {id: "facebook", name: "Facebook"},
+                {id: "advertisement", name: "Advertisement"}
+            ];
+    
             });
