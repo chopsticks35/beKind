f674a26617acf932dec0aba23f2ea6a2498fdbbc
Ti Wegmeyer <tiwegmeyeryoga@gmail.com>
2016-02-12 06:09:24 -0700

    phpifying attempt

--- mainScript.js
+++ mainScript.js
@@ -18,8 +18,112 @@ angular.module("myApp")
                 {id: "advertisement", name: "Advertisement"}
                 ];    
         
+//    $scope.submitForm = function(){
+//        
+//        //assethod="post" action="mailer.php"ing values
+//        if ($scope.name){var name = $scope.name;}
+//        else {var name = null;}
+//        if ($scope.email){var email = $scope.email;}
+//        else {var email = null;}
+//        if ($scope.hear){var hear = $scope.hear;}
+//        else {var hear = null;}
+//        if ($scope.wantNewsletter){var wantNewsletter = $scope.wantNewsletter;}
+//        else {var wantNewletter = false;}
+//        if ($scope.message){var message = $scope.message;}
+//        else {var message = nulle;}
+//        
+//        //build object
+//        $scope.myForm.$add({
+//            name:name,
+//            email: email,
+//            hear: hear,
+//            wantNewsletter: wantNesletter,
+//            message: message
+//            
+//        }).then(function(ref){
+//            //assign primary key
+//            var id = ref.key();
+//            console.log("added myForm with id" + id);
+//            
+//            //clear form
+//            clearFields();
+//            
+//            
+//            //send message
+//         $scope.msg = 'Quote added';
+//            
+//        });
     
     });
+
+//        app.controller('ContactController', function ($scope, $http) {
+//        
+//            $scope.result = 'hidden'
+//        
+//            $scope.resultMessage;
+//        
+//            $scope.formData; //formData is an object holding the name, email, subject, and message
+//        
+//            $scope.submitButtonDisabled = false;
+//        
+//            $scope.submitted = false; //used so that form errors are shown only after the form has been submitted
+//        
+//            $scope.submit = function(contactform) {
+//        
+//                $scope.submitted = true;
+//    
+//                $scope.submitButtonDisabled = true;
+//        
+//                if (contactform.$valid) {
+//    
+//                    $http({
+//        
+//                        method  : 'POST',
+//        
+//                        url     : 'contact-form.php',
+//        
+//                        data    : $.param($scope.formData),  //param method from jQuery
+//        
+//                        headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  //set the headers so angular passing info as form data (not request payload)
+//        
+//                    }).success(function(data){
+//        
+//                        console.log(data);
+//        
+//                        if (data.success) { //success comes from the return json object
+//        
+//                            $scope.submitButtonDisabled = true;
+//        
+//                            $scope.resultMessage = data.message;
+//        
+//                            $scope.result='bg-success';
+//     
+//                        } else {
+//       
+//                            $scope.submitButtonDisabled = false;
+//       
+//                            $scope.resultMessage = data.message;
+//        
+//                            $scope.result='bg-danger';
+//       
+//                        }
+//       
+//                    });
+//        
+//                } else {
+//        
+//                    $scope.submitButtonDisabled = false;
+//        
+//                    $scope.resultMessage = 'Failed <img src="http://www.chaosm.net/blog/wp-includes/images/smilies/icon_sad.gif" alt=":(" class="wp-smiley">  Please fill out all the fields.';
+//        
+//                    $scope.result='bg-danger';
+//        
+//                }
+//        
+//            }
+//        
+//        });
+
 
 
 //<script>angular.module('myScheduleApp', ['infinite-scroll'])
