5fbfbafe3e1e9a14b6c6357367fc0cee63e6c743
Ti Wegmeyer <tiwegmeyeryoga@gmail.com>
2016-02-11 18:45:01 -0700

    working on schedule

--- mainScript.js
+++ mainScript.js
@@ -16,6 +16,50 @@ angular.module("myApp")
                 {id: "web", name: "Web Search"},
                 {id: "facebook", name: "Facebook"},
                 {id: "advertisement", name: "Advertisement"}
-            ];
+                ];    
+        
     
-            });
+    });
+
+
+//<script>angular.module('myScheduleApp', ['infinite-scroll'])
+//angular.module('myScheduleApp')	
+//    .controller('myScheduleController', ['$scope','$timeout', function($scope,$timeout){
+//
+//	var today = new Date();
+//	$scope.images = [];
+//	$scope.counter = 0;
+//
+//	$scope.loadMore = function(){
+//
+//    	for(var i = 0; i <3; i++) {
+//    		today.setDate(today.getDate()+1);
+//    		var dd = today.getDate();
+//    		var mm = today.getMonth()+1;
+//    		var y = today.getFullYear();
+//    		var formattedDate = mm+'/'+dd+'/'+y;
+//    		var date = {
+//    			count: $scope.counter++,
+//    			day: formattedDate,
+//    			visibility: false,
+//    			agendaVisibility: true,
+//    			button: 'Add Class to Schedule',
+//    			agenda: 'a',
+//			} 
+//      		$scope.images.push(date);
+//   		}
+//	}
+//	$scope.addAgenda = function(index, event){
+//		$scope.images[index].visibility = true;
+//		$timeout(function() {
+//					event.target.nextElementSibling.focus();
+//				});
+//	}
+//	$scope.exitText = function(index){
+//		$scope.images[index].agenda = $scope.images[index].appointment
+//		$scope.images[index].appointment = ''
+//		$scope.images[index].visibility = false;
+//		$scope.images[index].agendaVisibility = false;
+//		$scope.images[index].button = "Edit Class";
+//	}
+//}]);</script>
